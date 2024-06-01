64bd48bf6fe1e4a274c5457e7df5952264f609d3
Karthikeya Udupa <karthikeyaudupa@gmail.com>
2024-06-01 11:09:47 +0200

    chore: fix vercel function
    Signed-off-by: Karthikeya Udupa <karthikeyaudupa@gmail.com>

--- api/forward.ts
+++ api/forward.ts
@@ -5,11 +5,9 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const urls = process.env.FORWARD_URLS?.split(",") || [];
 
   if (!urls.length) {
-    return res
-      .status(500)
-      .json({
-        error: "No URLs provided in the environment variable FORWARD_URLS",
-      });
+    return res.status(500).json({
+      error: "No URLs provided in the environment variable FORWARD_URLS",
+    });
   }
 
   try {
