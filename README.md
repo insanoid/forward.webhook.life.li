33a48b967934b7764ac82b274ed5c41978134192
Karthikeya Udupa <karthikeyaudupa@gmail.com>
2024-06-01 10:23:56 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# forward.webhook.life.li
+
+Forwards requests from a given SendGrid webhook to your localhost URLs without touching sendgrid.
---
+++ api/forward.tsx
@@ -0,0 +1,37 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import fetch from "node-fetch";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const urls = process.env.FORWARD_URLS?.split(",") || [];
+
+  if (!urls.length) {
+    return res
+      .status(500)
+      .json({
+        error: "No URLs provided in the environment variable FORWARD_URLS",
+      });
+  }
+
+  try {
+    const responses = await Promise.all(
+      urls.map((url) =>
+        fetch(url, {
+          method: req.method,
+          headers: req.headers as any,
+          body:
+            req.method !== "GET" && req.method !== "HEAD"
+              ? req.body
+              : undefined,
+        })
+      )
+    );
+
+    const results = await Promise.all(
+      responses.map((response) => response.text())
+    );
+
+    return res.status(200).json({ results });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
