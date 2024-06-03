3111809e8c3716a0c0b342d58443c04e32e96edb
Karthikeya Udupa <karthikeyaudupa@gmail.com>
2024-06-03 13:37:42 +0200

    fix: update the logic to handle forms
    Signed-off-by: Karthikeya Udupa <karthikeyaudupa@gmail.com>

--- api/forward.ts
+++ api/forward.ts
@@ -5,27 +5,52 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const urls = process.env.FORWARD_URLS?.split(",") || [];
 
   if (!urls.length) {
-    return res.status(500).json({
-      error: "No URLs provided in the environment variable FORWARD_URLS",
-    });
+    return res
+      .status(500)
+      .json({
+        error: "No URLs provided in the environment variable FORWARD_URLS",
+      });
   }
 
   try {
-    delete req.headers.host;
-    delete req.headers.referer;
-    console.log("body", req.body);
+    const headers = { ...req.headers };
+
+    // Remove host header to avoid issues with cross-domain requests
+    delete headers.host;
+
+    let body;
+    if (req.method !== "GET" && req.method !== "HEAD") {
+      if (
+        req.headers["content-type"]?.includes(
+          "application/x-www-form-urlencoded"
+        )
+      ) {
+        const formData = new URLSearchParams(req.body).toString();
+        body = formData;
+      } else if (req.headers["content-type"]?.includes("multipart/form-data")) {
+        body = req.body;
+      } else {
+        body = JSON.stringify(req.body);
+      }
+    }
+
     const responses = await Promise.all(
       urls.map((url) =>
         fetch(url, {
           method: req.method,
-          headers: req.headers as any,
-          body: req.body,
+          headers: headers as any,
+          body: body,
         })
       )
     );
 
     const results = await Promise.all(
-      responses.map((response) => response.text())
+      responses.map(async (response) => ({
+        url: response.url,
+        status: response.status,
+        headers: response.headers.raw(),
+        body: await response.text(),
+      }))
     );
 
     return res.status(200).json({ results });
